f27301421901a882c78d78eb3fe218437651f90f
Andrey <vijevitovandrey@ya.ru>
2025-04-30 08:36:37 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar.tsx';
-import About from './Pages/About.tsx';
-import Favorites from './Pages/Favorites.tsx';
-import Home from './Pages/Home.tsx';
-import MovieDetail from './Pages/MovieDetail.tsx';
-import { useThemeStore } from './Store/useThemeStore'; // Импортируем хранилище темы
-import { useEffect } from 'react';
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar.tsx";
+import About from "./Pages/About.tsx";
+import Favorites from "./Pages/Favorites.tsx";
+import Home from "./Pages/Home.tsx";
+import MovieDetail from "./Pages/MovieDetail.tsx";
+import { useThemeStore } from "./Store/useThemeStore";
+import { useEffect } from "react";
 
 const App = () => {
   const { theme } = useThemeStore();
 
   useEffect(() => {
-    document.documentElement.setAttribute('data-theme', theme);
+    document.documentElement.setAttribute("data-theme", theme);
   }, [theme]);
 
   return (
@@ -29,4 +29,4 @@ const App = () => {
   );
 };
 
-createRoot(document.getElementById('root')!).render(<App />);
+createRoot(document.getElementById("root")!).render(<App />);
